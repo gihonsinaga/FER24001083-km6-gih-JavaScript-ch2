de51a6b24fb7d6d3192c452298104b23e7f6ee70
gihon sinaga <89863525+gihonsinaga@users.noreply.github.com>
2024-03-27 18:55:15 +0700

    Update w3resource.js

--- w3resource.js
+++ w3resource.js
@@ -1,19 +1,19 @@
 // Function to get the last n elements of an array
 var last = function (array, n) {
   // Check if the input array is null, return undefined if true
-  if (array == null) return void 0;
+  if (array == null) return undefined;
 
   // Check if the value of n is null, return the last element of the array if true
-  if (n == null) return array[array.length - 1];
+  if (n == null) return array[array.length - 2];
+  // 4-2 = 2
 
   // Use the slice method to get the last n elements of the array
   // Math.max is used to ensure the starting index is not negative
   return array.slice(Math.max(array.length - n, 0));
+  // 4 -2 = (2,0)
 };
 
 // Testing the function with various cases
 console.log(last([]));
 console.log(last([7, 9, 0, -2]));
-console.log(last([7, 9, 0, -2], 3));
-console.log(last([7, 9, 0, -2], 7));
-console.log(last([0], 9));
+console.log(last([7, 9, 0, -2], 2));
